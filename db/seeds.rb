bbc24d6152b85fe3b891a29981c2c05818f875eb
cosenocs <cosenocs@yandex.com>
2023-05-22 23:21:06 +0600

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(
+  name: 'Бабур Ибрагимжанов',
+  business: 'ИП Ибрагимжанов',
+  phone: '87075111011',
+  role: 'business',
+  email: 'babur@example.com',
+  password: 'secret'
+)
+
+User.create!(
+  name: 'Зафар',
+  business: 'ИП Зафар',
+  phone: '87751607160',
+  role: 'admin',
+  email: 'zafar@example.com',
+  password: 'secret'
+)
